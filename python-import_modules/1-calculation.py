1f5e14b1c04e21ce186441c0a7dff741450b58fb
qemer26 <qemerqehremanova817@gmail.com>
2025-05-01 13:01:04 +0000

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,7 +2,6 @@
 from add_1 import add
 
 if __name__ == "__main__":
-    a=10
-    b=5
-    result_add = add(a, b)
-    print("{} + {} = {}".format(a, b, result_add))i
+    a = 10i
+    b = 5   
+    print("{} + {} = {}".format(a, b, add(a, b)))
