803d6facb732620fedb794ac354079f27623293a
qemer26 <qemerqehremanova817@gmail.com>
2025-05-01 12:56:42 +0000

    task2

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from add_1 import add
+
 if __name__ == "__main__":
     a=10
     b=5
     result_add = add(a, b)
-    print("{} + {} = {}".format(a, b, result_add))
+    print("{} + {} = {}".format(a, b, result_add))i
