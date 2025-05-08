33a2d5facc16f33f89b941f6d0ce3c751b2162ed
qemer26 <qemerqehremanova817@gmail.com>
2025-05-08 13:55:22 +0000

    11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        del my_list[idx]
-    return my_list
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
